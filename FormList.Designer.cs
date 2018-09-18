0bf11004b611492cc860d7be9e1c2b2e979bebf8
Frankie Barrios <softwaredevfb@gmail.com>
2018-09-17 20:26:31 -0500

    Main files only for Sports App

---
+++ FormList.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace SportsEx5App
+{
+    partial class FormList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormList));
+            this.cmbType = new System.Windows.Forms.ComboBox();
+            this.lstBxSports = new System.Windows.Forms.ListBox();
+            this.lblHeading = new System.Windows.Forms.Label();
+            this.lblSportInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbType
+            // 
+            this.cmbType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbType.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbType.FormattingEnabled = true;
+            this.cmbType.Items.AddRange(new object[] {
+            "Tennis ",
+            "Football"});
+            this.cmbType.Location = new System.Drawing.Point(85, 50);
+            this.cmbType.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbType.Name = "cmbType";
+            this.cmbType.Size = new System.Drawing.Size(209, 28);
+            this.cmbType.TabIndex = 0;
+            this.cmbType.SelectedIndexChanged += new System.EventHandler(this.cmbType_SelectedIndexChanged);
+            // 
+            // lstBxSports
+            // 
+            this.lstBxSports.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstBxSports.FormattingEnabled = true;
+            this.lstBxSports.ItemHeight = 20;
+            this.lstBxSports.Location = new System.Drawing.Point(85, 112);
+            this.lstBxSports.Margin = new System.Windows.Forms.Padding(2);
+            this.lstBxSports.Name = "lstBxSports";
+            this.lstBxSports.Size = new System.Drawing.Size(209, 204);
+            this.lstBxSports.TabIndex = 1;
+            this.lstBxSports.SelectedIndexChanged += new System.EventHandler(this.lstBxSports_SelectedIndexChanged);
+            // 
+            // lblHeading
+            // 
+            this.lblHeading.AutoSize = true;
+            this.lblHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHeading.Location = new System.Drawing.Point(28, 378);
+            this.lblHeading.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblHeading.Name = "lblHeading";
+            this.lblHeading.Size = new System.Drawing.Size(209, 37);
+            this.lblHeading.TabIndex = 2;
+            this.lblHeading.Text = "Sport Details:";
+            // 
+            // lblSportInfo
+            // 
+            this.lblSportInfo.AutoSize = true;
+            this.lblSportInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSportInfo.Location = new System.Drawing.Point(32, 444);
+            this.lblSportInfo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblSportInfo.Name = "lblSportInfo";
+            this.lblSportInfo.Size = new System.Drawing.Size(65, 20);
+            this.lblSportInfo.TabIndex = 3;
+            this.lblSportInfo.Text = "Details";
+            // 
+            // FormList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(383, 538);
+            this.Controls.Add(this.lblSportInfo);
+            this.Controls.Add(this.lblHeading);
+            this.Controls.Add(this.lstBxSports);
+            this.Controls.Add(this.cmbType);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FormList";
+            this.Text = "Sport Info List";
+            this.Load += new System.EventHandler(this.FormList_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbType;
+        private System.Windows.Forms.ListBox lstBxSports;
+        private System.Windows.Forms.Label lblHeading;
+        private System.Windows.Forms.Label lblSportInfo;
+    }
+}
+
---
+++ FormList.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TeamsLib;
+using SportSubClass;
+
+/// <summary>
+/// Developer:  Frankie Barrios 
+/// Date:       9/17/2018
+/// Purpose:    Tester Ex 5 App
+/// </summary>
+namespace SportsEx5App
+{
+   
+    public partial class FormList : Form
+    {
+        //Class Fields
+        Tennis[] tenAry;
+        Football[] footAry;
+
+        public FormList()
+        {
+            InitializeComponent();
+        }
+
+        private void FormList_Load(object sender, EventArgs e)
+        {
+            lblHeading.Visible = false;
+            lblSportInfo.Visible = false;
+
+            tenAry = new Tennis[3];
+            footAry = new Football[3];
+
+            tenAry[0] = new Tennis("Tennis", "Racketeers", "Coach Fred", "Raquet", "Rod Laver Arena", 40);
+            tenAry[1] = new Tennis("Tennis", "Ballers", "Coach Tim", "Ball", "Xtreme Arena", 40);
+            tenAry[2] = new Tennis("Tennis", "Bulls", "Coach Mike", "Sweatband", "Bulls Arena", 40);
+
+            footAry[0] = new Football("Football", "Chargers", "Coach Ed", "Astro Turf", "Chicago", 300);
+            footAry[1] = new Football("Football", "Packers", "Coach Jim", "Asphalt", "St.Louis", 350);
+            footAry[2] = new Football("Football", "Bears", "Coach Al", "Green", "New York", 400);
+
+        }
+        private void cmbType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lstBxSports.Items.Clear();
+            lblHeading.Visible = false;
+            lblSportInfo.Visible = false;
+
+            if(cmbType.SelectedIndex == 0)
+            {
+                foreach(Tennis tn in tenAry)
+                {
+                    if (tn != null)
+                        lstBxSports.Items.Add(tn.TeamName);
+                }//End of foreach
+
+            }//End of if
+            else if (cmbType.SelectedIndex == 1)
+            {
+                foreach(Football ft in footAry)
+                {
+                    if (ft != null)
+                        lstBxSports.Items.Add(ft.TeamName);
+                }//End of foreach
+            }//End of else if
+        }//End of private void cmbType_SelectedIndexChanged
+
+        private void lstBxSports_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblHeading.Visible = true;
+            lblSportInfo.Visible = true;
+
+            if (cmbType.SelectedIndex == 0) //Tennis
+            {
+                foreach (Tennis tn in tenAry)
+                {
+                    if (tn.TeamName.Equals(lstBxSports.SelectedItem))
+                    {
+                        lblHeading.Visible = true;
+                        lblSportInfo.Visible = true;
+                        lblSportInfo.Text = tn.ToString();
+                    }//End of if
+                }
+            }//End of foreach
+            else if (cmbType.SelectedIndex == 1)//Football
+            {
+                foreach (Football ft in footAry)
+                {
+                    if (ft.TeamName.Equals(lstBxSports.SelectedItem))
+                        lblSportInfo.Text = ft.ToString();
+                }//End of foreach
+            }//End of else if
+
+            }//End of if
+        }//End of private void lstBxSports_SelectedIndexChanged
+    }//End of Class
+//End of Solution
